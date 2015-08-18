84cdf69ff503143f68d3e39cd2f303b4a19ef032
clrnd <welcometothechango@gmail.com>
2015-08-18 16:42:09 -0300

    bytestring builders for lol fast

--- src/Discogs/Store.hs
+++ src/Discogs/Store.hs
@@ -1,12 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Discogs.Store where
 
-import           Data.Monoid
-import           Control.Monad
-import           Database.PostgreSQL.Simple
-import           Database.PostgreSQL.Simple.Copy
-import qualified Data.ByteString.Char8 as B
-import           Data.ByteString.Char8 (ByteString)
+import Data.Monoid
+import Data.List (intersperse)
+import Control.Monad
+import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.Copy
+import Data.ByteString (ByteString)
+import Data.ByteString.Char8 (pack)
+import Data.ByteString.Lazy (toStrict)
+import Data.ByteString.Internal (w2c)
+import Data.ByteString.Builder as B (Builder, toLazyByteString, charUtf8)
+import Data.ByteString.Builder.Prim as P
 
 
 class Storable a where
@@ -17,7 +22,7 @@ class Storable a where
 
 store :: (Show a, Storable a) => String -> [a] -> IO ()
 store cons ars = do
-    conn <- connectPostgreSQL $ B.pack cons
+    conn <- connectPostgreSQL $ pack cons
     copy_ conn $ getQuery ars
 
     forM_ ars $ \a ->
@@ -35,29 +40,45 @@ store cons ars = do
 
 
 class Escapable b where
-    escape :: b -> ByteString
-    escapeList :: [b] -> ByteString
+    escape :: b -> Builder
+    escapeList :: [b] -> Builder
 
 
 instance Escapable ByteString where
-    escape "" = "\\N"
-    escape s = B.concatMap replace s
+    escape "" = B.charUtf8 '\\' <> B.charUtf8 'N'
+    escape s = primMapByteStringBounded (w2c >$< replace) s
       where
-        replace '\\' = ""
-        replace '\n' = "\\n"
-        replace '\t' = "\\t"
-        replace x = B.singleton x
+        replace =
+            condB (== '\\') emptyB $
+            condB (== '\n') (fixed2 ('\\', 'n')) $
+            condB (== '\t') (fixed2 ('\\', 't')) $
+            P.charUtf8
 
-    escapeList [] = "\\N"
-    escapeList l = "{" <> (B.intercalate "," $ map wrap l) <> "}"
+    escapeList [] = B.charUtf8 '\\' <> B.charUtf8 'N'
+    escapeList l = B.charUtf8 '{' <>
+                       (mconcat . intersperse (B.charUtf8 ',') . map wrap $ l) <>
+                   B.charUtf8 '}'
       where
-        wrap s = "\"" <> B.concatMap replace s <> "\""
-        replace '\"' = "\\\\\""
-        replace '\\' = ""
-        replace '\n' = "\\n"
-        replace '\t' = "\\t"
-        replace x = B.singleton x
+        wrap s = B.charUtf8 '\"' <>
+                     primMapByteStringBounded (w2c >$< replace) s <>
+                 B.charUtf8 '\"'
+        replace =
+            condB (== '\\') emptyB $
+            condB (== '\"') (fixed3 ('\\', ('\\', '\"'))) $
+            condB (== '\n') (fixed2 ('\\', 'n')) $
+            condB (== '\t') (fixed2 ('\\', 't')) $
+            P.charUtf8
 
 
-escapeRow :: [ByteString] -> ByteString
-escapeRow as = (B.intercalate "\t" as) `B.snoc` '\n'
+{-# INLINE fixed2 #-}
+fixed2 :: (Char, Char) -> BoundedPrim Char
+fixed2 x = liftFixedToBounded $ const x >$< char7 >*< char7
+
+{-# INLINE fixed3 #-}
+fixed3 :: (Char, (Char, Char)) -> BoundedPrim Char
+fixed3 x = liftFixedToBounded $ const x >$< char7 >*< char7 >*< char7
+
+
+escapeRow :: [Builder] -> ByteString
+escapeRow as = toStrict . toLazyByteString $
+    mconcat (intersperse (B.charUtf8 '\t') as) <> B.charUtf8 '\n'
